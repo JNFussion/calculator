d0f06930ba28fe54251852cac10cf98a1fe43bac
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-09-19 20:41:41 +0200

    finished

--- calculator.js
+++ calculator.js
@@ -6,19 +6,29 @@ const dotBtn = document.getElementById('dot')
 const clearBtn = document.getElementById('clear');
 const backBtn = document.getElementById('back');
 const equalBtn = document.getElementById('equal');
+let operand1 = null;
+let operand2 = null;
+let operator = null;
 
 
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
 function add(a, b) {
-    return a + b;
+
+    return round(a + b, 4);
 }
 function subtract(a, b) {
-    return a - b;
+
+    return round(a - b, 4);
 }
 function multiply(a, b) {
-    return a * b;
+
+    return round(a * b, 4);
 }
 function divide(a, b) {
-    return a / b;
+    return round(a / b, 4);
 }
 function operate(operator, a, b) {
 
@@ -30,7 +40,7 @@ function operate(operator, a, b) {
         return multiply(a, b);
     } else if (operator === "/") {
         if (b !== 0) {
-            return divide(a / b);
+            return divide(a, b);
         } else {
             alert("Cannot divide by 0");
             reset();
@@ -43,8 +53,11 @@ function operate(operator, a, b) {
 function reset() {
     location.reload();
 }
-function thereIsOperator(str) {
+function isThereOperator(str) {
     return str !== null && str.match(/[+-/*]/g) !== null;
+}
+function isThereDot(str) {
+    return str !== null && str.match(/[.]/g) !== null;
 }
 function setOperationDisplay(simbol, number1, number2) {
 
@@ -55,151 +68,143 @@ function setOperationDisplay(simbol, number1, number2) {
     } else if (number1 !== null && simbol === null && number2 === null) {
         operationDisplay.textContent = number1 + " ";
     } else {
-        return operationDisplay.textContent = 'A';
+        return operationDisplay.textContent = '';
     }
 }
-/*
-function getFirstOperands (str){ 
-    return str.split(" ")[0];
+function addNumber(btn) {
+    if (!isThereOperator(operator)) {
+        if (operand1) {
+            operand1 += btn.textContent
+        } else {
+            operand1 = btn.textContent
+        }
+    } else {
+        if (operand2) {
+            operand2 += btn.textContent
+        } else {
+            operand2 = btn.textContent
+        }
+    }
+    setOperationDisplay(operator, operand1, operand2);
 }
-function getSecondOperands (str){
-    return str.split(" ")[2];
+function addOperator(btn) {
+    if (!isThereOperator(operator) && operand1) {
+        operator = btn.textContent;
+    } else if (isThereOperator(operator) && operand1 !== null && operand2 !== null) {
+        operand1 = operate(operator, +operand1, +operand2);
+        operator = btn.textContent;
+        operand2 = null;
+    }
+    setOperationDisplay(operator, operand1, operand2);
 }
-function getOperator(str){
-    return str.split(" ")[1];
+function addDot(btn) {
+    if (!isThereOperator(operator) && operand1 !== null &&
+        !isThereDot(operand1)) {
+        operand1 += btn.textContent;
+    } else {
+        if (operand2 !== null && !isThereDot(operand2)) {
+            operand2 += btn.textContent;
+        }
+    }
+    setOperationDisplay(operator, operand1, operand2)
 }
-function isOperator(str){
-    if(str === "+"){
-        return true;
-    }else if(str === "-"){
-        return true;
-    }else if(str === "*"){
-        return true
-    }else if(str === "/"){
-        return true;
-    }else{
-        return false;
+function undo() {
+    if (!isThereOperator(operator) && operand1 !== null) {
+        operand1 = operand1.slice(0, -1);
+        if (operand1 === '') {
+            operand1 = null;
+        }
+    } else {
+        if (operand2 !== null) {
+            operand2 = operand2.slice(0, -1);
+
+            if (operand2 === '') {
+                operand2 = null;
+            }
+        } else {
+            operator = null;
+        }
+    }
+    setOperationDisplay(operator, operand1, operand2);
+}
+function equal() {
+    if (isThereOperator(operator) && operand1 !== null && operand2 !== null) {
+        resultDisplay.textContent = operate(operator, +operand1, +operand2);
+        operator = null;
+        operand1 = null;
+        operand2 = null;
     }
 }
-
-function isPreviusOperator(str){
-    return isOperator(str.charAt(str.length -1));
-}
-
-function main(){
-    const result = document.querySelector('#result');
-    const operation = document.querySelector('#operations');
-    const numbers = document.querySelectorAll('.number');
-    const operators = document.querySelectorAll('.operator');
-    const clear = document.getElementById('clear');
-    const back = document.querySelector('#back');
-    const equal = document.querySelector('#equal')
-    let lastInput = null;
-
-    numbers.forEach(button => {
-        button.addEventListener('click', () =>{
-            if(lastInput === '='){
-                operation.textContent = button.textContent;  
-            }else if(isPreviusOperator(operation.textContent)){
-                operation.textContent +=" " + button.textContent;
-            }else{
-                operation.textContent += button.textContent;  
-            }
-            lastInput = button.textContent;
-        })
-    })
-
-    operators.forEach(operator =>{
-        
-        operator.addEventListener('click', () =>{
-            if(thereIsOperator(operation.textContent) && isOperator(lastInput) ){
-                let operator = getOperator(operation.textContent);
-                let operand1 = getFirstOperands(operation.textContent)
-                let operand2 = getSecondOperands(operation.textContent)
-                result.textContent = operate(operator,+operand1, +operand2)
-                operation.textContent = result.textContent;
-                lastInput = operator.textContent;
-            }else if(!isOperator(lastInput)){
-                operation.textContent +=" "+ operator.textContent;
-                lastInput = operator.textContent;
-            }
-            
-        })
-    })
-
-    
-
-
-    clear.addEventListener('click', reset);
-    back.addEventListener('click', () =>{
-        operation.textContent = operation.textContent.slice(0,-1);
-    })
-    equal.addEventListener('click',() =>{
-        let operator = getOperator(operation.textContent);
-        let operand1 = getFirstOperands(operation.textContent)
-        let operand2 = getSecondOperands(operation.textContent)
-        result.textContent = operate(operator,+operand1, +operand2)
-        lastInput = equal.textContent;
-    })
-}
-*/
 function main() {
-    let operand1 = null;
-    let operand2 = null;
-    let operator = null;
+
+    //Adds events to all the buttons
 
     numbersBtn.forEach(button => {
         button.addEventListener('click', () => {
-            if (!thereIsOperator(operator)) {
-                if (operand1) {
-                    operand1 += button.textContent
-                } else {
-                    operand1 = button.textContent
-                }
-            } else {
-                if (operand2) {
-                    operand2 += button.textContent
-                } else {
-                    operand2 = button.textContent
-                }
-            }
-            console.log(operator, operand1, operand2);
-            setOperationDisplay(operator, operand1, operand2);
+            addNumber(button)
         })
     })
     operatorsBtn.forEach(op => {
         op.addEventListener('click', () => {
-            if (!thereIsOperator(operator) && operand1) {
-                operator = op.textContent;
-            } else if (thereIsOperator(operator) && operand1 !== null && operand2 !== null) {
-                operand1 = operate(operator, +operand1, +operand2);
-                operator = op.textContent;
-                operand2 = null;
-            }
-            console.log(operator, operand1, operand2);
-            setOperationDisplay(operator, operand1, operand2);
+            addOperator(op);
         })
     })
-    equalBtn.addEventListener('click', () => {
-        if (thereIsOperator(operator) && operand1 !== null && operand2 !== null) {
-            resultDisplay.textContent = operate(operator, +operand1, +operand2);
-            operator = null;
-            operand1 = null;
-            operand2 = null;
+    dotBtn.addEventListener('click', () => {
+        addDot(dotBtn);
+    })
+
+
+    equalBtn.addEventListener('click', equal);
+    backBtn.addEventListener('click', undo);
+    clear.addEventListener('click', reset);
+
+
+    //Adds input by keyboards
+
+    window.addEventListener('keydown', function (e) {
+        const button = document.querySelector(`button[data-key="${e.key}"]`);
+        if (!button) {
+            return;
         }
 
+        if (button.className === "number") {
+            addNumber(button);
+        } else if (button.className === "operator") {
+            addOperator(button);
+        } else {
+            switch (button.getAttribute('id')) {
+                case 'dot':
+                    addDot(button);
+                    break;
+                case 'equal':
+                    equal();
+                    break;
+                case 'back':
+                    undo();
+                    break;
+            }
+        }
     })
-    dotBtn.addEventListener('click', () =>{
-        
-    })
+
+    // Adds border of color white when hover over buttons
+
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(btn => btn.addEventListener('mouseenter', () => {
+        if (btn.className === 'operator') {
+            btn.classList.add('border-white-operators')
+        } else {
+            btn.classList.add('border-white-numbers')
+        }
 
 
-
-
-
-
-
-    clear.addEventListener('click', reset);
+    }))
+    allButtons.forEach(btn => btn.addEventListener('mouseleave', () => {
+        if (!btn.className.search('operator')) {
+            btn.classList.remove('border-white-operators')
+        } else {
+            btn.classList.remove('border-white-numbers')
+        }
+    }))
 }
 
 
